adc218f8d3eb59b0e44bc17476c6906a87aaa9bf
shahriar1100 <sahariarsikder.ss@gmail.com>
2026-04-24 23:46:05 +0600

    all done right now

--- js/script.js
+++ js/script.js
@@ -9,5 +9,24 @@ btn.addEventListener("click", ()=>{
     if(email.value === "" || password.value === ""){
         msg.innerText = "All Feild Required"
         msg.style.color = "tomato"
+    }else if(!email.value.includes("@")){
+        msg.innerText = "@ must be use"
+        msg.style.color = "tomato"
+    }else if(password.value.length <= 6){
+        msg.innerText = "password is too short"
+        msg.style.color = "tomato"
+    }
+    else if(password.value.length >= 6 && password.value.length <= 10){
+        msg.innerText = "Medium Password"
+        msg.style.color = "orange"
+    }else if(!/[A-Z]/.test(password.value)){
+        msg.innerText = "At least one charecter use UperCase"
+        msg.style.color = "tomato"
+    }else if(!/[0-9]/.test(password.value)){
+        msg.innerText = "At least one charecter use Number"
+        msg.style.color = "tomato"
+    }else{
+         msg.innerText = "Sucessfully login"
+        msg.style.color = "green"
     }
 })
